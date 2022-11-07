3c2ec181de10f53bd7287291bab0bc8b5ce3b9fa
hebato <joten3434@gmail.com>
2022-11-07 09:35:58 +0100

    Initial Commit

---
+++ githubProject0/githubProject0App.swift
@@ -0,0 +1,17 @@
+//
+//  githubProject0App.swift
+//  githubProject0
+//
+//  Created by Jakub N on 07/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct githubProject0App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
