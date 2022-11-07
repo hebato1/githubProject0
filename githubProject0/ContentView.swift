9b884c2b2b6e0b38ab5ab2e9c0ee57cd70683c70
hebato <joten3434@gmail.com>
2022-11-07 18:20:30 +0100

    koniec

--- githubProject0/ContentView.swift
+++ githubProject0/ContentView.swift
@@ -12,42 +12,76 @@ struct Box {
     let title1, title2, imageURL: String
 }
 
+struct Box2 {
+    var id: Int
+    let title1, title2, imageURL: String
+}
+
 struct ContentView: View {
     
     let boxes:[Box] = [
         Box(id: 0, title1: "An unknown journey", title2: "Story about him and her.", imageURL: "zd1"),
-        Box(id: 1, title1: "Photo2", title2: "aaaa", imageURL: "zd2"),
-        Box(id: 2, title1: "Photo3", title2: "aaaa", imageURL: "zd3"),
-        Box(id: 3, title1: "Photo4", title2: "aaaa", imageURL: "zd4"),
-        Box(id: 4, title1: "Photo5", title2: "aaaa", imageURL: "zd5")
+        Box(id: 1, title1: "An unknown journey", title2: "Story about him and her.", imageURL: "zd2"),
+        Box(id: 2, title1: "An unknown journey", title2: "Story about him and her.", imageURL: "zd3"),
+        Box(id: 3, title1: "An unknown journey", title2: "Story about him and her.", imageURL: "zd4"),
+        Box(id: 4, title1: "An unknown journey", title2: "Story about him and her.", imageURL: "zd5")
     ]
+    
+    let boxesBigger:[Box2] = [
+        Box2(id: 0, title1: "An unknown journey", title2: "Story about him and her.", imageURL: "duze1"),
+        Box2(id: 1, title1: "An unknown journey", title2: "Story about him and her.", imageURL: "duze2"),
+        Box2(id: 2, title1: "An unknown journey", title2: "Story about him and her.", imageURL: "duze3"),
+        Box2(id: 3, title1: "An unknown journey", title2: "Story about him and her.", imageURL: "duze4"),
+        Box2(id: 4, title1: "An unknown journey", title2: "Story about him and her.", imageURL: "duze5")
+    ]
+    
+    
     
     var body: some View {
         ZStack{
             Color(red: 81/255, green: 113/255, blue: 116/255)
                 .ignoresSafeArea()
             VStack{
-                Text("Free stories")
-                    .frame(maxWidth: .infinity, alignment: .leading)
-                    .font(.system(size: 30, weight: .semibold))
-                Text("Enjoy our free collection of audio stories.")
-                    .frame(maxWidth: .infinity, alignment: .leading)
-                    .font(.system(size: 14))
-                ScrollView(.horizontal){
-                    HStack{
-                        ForEach(boxes, id: \.id) { box in
-                            BoxView(box: box)
+                VStack {
+                    VStack{
+                        Text("Free stories")
+                            .frame(maxWidth: .infinity, alignment: .leading)
+                            .font(.system(size: 30, weight: .semibold))
+                        Text("Enjoy our free collection of audio stories.")
+                            .frame(maxWidth: .infinity, alignment: .leading)
+                            .font(.system(size: 14))
+                        ScrollView(.horizontal){
+                            HStack{
+                                ForEach(boxes, id: \.id) { box in
+                                    BoxView(box: box)
+                                }
+                            }
+                            
                         }
                     }
-                    
+                    VStack{
+                        Text("Top Premium Stories")
+                            .frame(maxWidth: .infinity, alignment: .leading)
+                            .font(.system(size: 30, weight: .semibold))
+                        Text("Our best stories for premium users.")
+                            .frame(maxWidth: .infinity, alignment: .leading)
+                            .font(.system(size: 14))
+                        ScrollView(.horizontal){
+                            HStack{
+                                ForEach(boxesBigger, id: \.id) { box in
+                                    BoxViewBig(boxBig: box)
+                                }
+                            }
+                            
+                        }
+                    }
                 }
+                
+                .padding(5)
                 Spacer()
             }
-            .padding(5)
-         
         }
         
-
             
 
     }
@@ -70,6 +104,36 @@ struct ContentView: View {
                     
                     
                     Text(box.title2)
+                        .fixedSize(horizontal: false, vertical: true)
+                        .font(.system(size: 8))
+                        .multilineTextAlignment(.leading)
+                        .frame(width: 150, alignment: .leading)
+                    
+                        
+            }
+        }
+    }
+    
+    
+    struct BoxViewBig: View {
+        let boxBig: Box2
+        
+        
+        var body: some View {
+                VStack{
+                    Image(boxBig.imageURL)
+                        .resizable()
+                        .frame(width: 150, height: 300)
+                        .cornerRadius(12)
+                    Text(boxBig.title1)
+                        .fixedSize(horizontal: false, vertical: true)
+                        .font(.system(size: 12, weight: .semibold))
+                        .multilineTextAlignment(.leading)
+                        .frame(width: 150, alignment: .leading)
+                    
+                    
+
+                    Text(boxBig.title2)
                         .fixedSize(horizontal: false, vertical: true)
                         .font(.system(size: 8))
                         .multilineTextAlignment(.leading)
